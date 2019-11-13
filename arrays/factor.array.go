64b30bda9fe1643de0188b523411c6cdf06c2162
Anton  Repin <arepin@cisco.com>
2019-11-13 13:18:24 -0800

    Factor extendable array added

---
+++ arrays/factor.array.go
@@ -0,0 +1,117 @@
+package arrays
+
+import (
+	"reflect"
+)
+
+type FactorArray struct {
+	arr 				[]interface{}
+	size    			uint32
+	factor				uint32
+	cap 				uint32
+	initialCap			uint32
+	genericType 		interface{}
+	genericIsPointer 	bool
+}
+
+type FactorOptions struct {
+	Factor 		uint32
+	InitialCap	uint32
+}
+
+func NewFactorArray(opt *FactorOptions) *FactorArray {
+	f := uint32(50)
+	c := uint32(8)
+	if opt != nil {
+		if opt.Factor != 0 { f = opt.Factor }
+		if opt.InitialCap != 0 { c = opt.InitialCap }
+	}
+	return &FactorArray{factor: f, cap: c, initialCap: c}
+}
+
+func (sa *FactorArray) Size() uint32 {
+	return sa.size
+}
+
+func (sa *FactorArray) Add(item interface{}) {
+	if sa.genericType == nil {
+		vt := reflect.ValueOf(item).Type().Kind()
+		if vt == reflect.Ptr {
+			sa.genericType = reflect.ValueOf(item).Elem().Type()
+			sa.genericIsPointer = true
+		} else {
+			sa.genericType = vt
+		}
+		sa.genericType = item
+	}
+	if sa.arr == nil {
+		sa.resize()
+		sa.arr[sa.size] = item
+		sa.size++
+		return
+	}
+	if sa.size == uint32(len(sa.arr)) {
+		sa.resize()
+	}
+	sa.arr[sa.size] = item
+	sa.size++
+}
+
+func (sa *FactorArray) Get(index uint32, receiver interface{}) {
+	if index > uint32(len(sa.arr) - 1) {
+		panic("out of bounds")
+	}
+	if sa.genericIsPointer {
+		reflect.ValueOf(receiver).Elem().Set(reflect.ValueOf(sa.arr[index]).Elem())
+	} else {
+		reflect.ValueOf(receiver).Elem().Set(reflect.ValueOf(sa.arr[index]))
+	}
+}
+
+func (sa *FactorArray) Remove(index uint32, receiver interface{}) {
+	if index > sa.size {
+		panic("out of bounds")
+	}
+	if receiver != nil && sa.genericIsPointer {
+		reflect.ValueOf(receiver).Elem().Set(reflect.ValueOf(sa.arr[index]).Elem())
+	} else if receiver != nil {
+		reflect.ValueOf(receiver).Elem().Set(reflect.ValueOf(sa.arr[index]))
+	}
+	// Shrink the slice by slicing original slice until the index
+	// and appending everything what after index + 1 with spread
+	if sa.size > 1 {
+		sa.arr = append(sa.arr[:index], sa.arr[index+1:]...)
+		sa.cap--
+	} else {
+		sa.arr = nil
+		sa.cap = sa.initialCap
+	}
+	sa.size--
+}
+
+func (sa *FactorArray) AddAtIndex(index uint32, item interface{}) {
+	if index > sa.size + sa.factor {
+		panic("out of bounds")
+	}
+	if index < uint32(len(sa.arr) - 1) {
+		sa.arr[index] = item
+		return
+	}
+	sa.resize()
+	sa.arr[index] = item
+}
+
+func (sa *FactorArray) allocate(size uint32) []interface{} {
+	return make([]interface{}, size)
+}
+
+func (sa *FactorArray) resize() {
+	if sa.cap < sa.initialCap {
+		sa.cap = sa.initialCap
+	}
+	newArr := sa.allocate(sa.size + sa.cap * sa.factor / 100)
+	if sa.arr != nil {
+		copy(newArr, sa.arr)
+	}
+	sa.arr = newArr
+}
---
+++ arrays/factor.array_test.go
@@ -0,0 +1,118 @@
+package arrays
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFactorArray_AddStruct(t *testing.T) {
+
+	a := NewFactorArray(nil)
+
+	a.Add(&TestStruct{name: "one"})
+	a.Add(&TestStruct{name: "two"})
+	a.Add(&TestStruct{name: "three"})
+
+	size := a.Size()
+	fmt.Printf("\nstruct array size %d", size)
+
+	one := &TestStruct{}
+	a.Get(0, one)
+	two := &TestStruct{}
+	a.Get(1, two)
+	three := &TestStruct{}
+	a.Get(2, three)
+
+	fmt.Printf("\nstruct values received [%+v] [%+v] [%+v]", one, two, three)
+
+	remOne := &TestStruct{}
+	a.Remove(0, remOne)
+	remTwo := &TestStruct{}
+	a.Remove(1, remTwo)
+	remThree := &TestStruct{}
+	a.Remove(0, remThree)
+
+	fmt.Printf("\nstruct values deleted [%+v] [%+v] [%+v]", remOne, remTwo, remThree)
+
+	size = a.Size()
+	fmt.Printf("\nstruct array size %d", size)
+}
+
+func TestFactorArray_AddString(t *testing.T) {
+
+	a := NewFactorArray(nil)
+
+	a.Add("one")
+	a.Add("two")
+	a.Add("three")
+
+	size := a.Size()
+	fmt.Printf("\nstring array size %d", size)
+
+	one := ""
+	a.Get(0, &one)
+	two := ""
+	a.Get(1, &two)
+	three := ""
+	a.Get(2, &three)
+
+	fmt.Printf("\nstring values received [%+v] [%+v] [%+v]", one, two, three)
+
+	remOne := ""
+	a.Remove(0, &remOne)
+	remTwo := ""
+	a.Remove(1, &remTwo)
+	remThree := ""
+	a.Remove(0, &remThree)
+
+	fmt.Printf("\nvalues deleted [%+v] [%+v] [%+v]", remOne, remTwo, remThree)
+
+	size = a.Size()
+	fmt.Printf("\narray size %d", size)
+}
+
+func TestFactorArray_AddInt(t *testing.T) {
+
+	a := NewFactorArray(nil)
+
+	a.Add(1)
+	a.Add(2)
+	a.Add(3)
+
+	size := a.Size()
+	fmt.Printf("\nstring array size %d", size)
+
+	one := 0
+	a.Get(0, &one)
+	two := 0
+	a.Get(1, &two)
+	three := 0
+	a.Get(2, &three)
+
+	fmt.Printf("\nstring values received [%+v] [%+v] [%+v]", one, two, three)
+
+	remOne := 0
+	a.Remove(0, &remOne)
+	remTwo := 0
+	a.Remove(1, &remTwo)
+	remThree := 0
+	a.Remove(0, &remThree)
+
+	fmt.Printf("\nvalues deleted [%+v] [%+v] [%+v]", remOne, remTwo, remThree)
+
+	size = a.Size()
+	fmt.Printf("\narray size %d", size)
+}
+
+func TestFactorArray_Fill(t *testing.T) {
+	a := NewFactorArray(nil)
+	for i:=0; i<1000; i++ {
+		a.Add(i)
+	}
+	fmt.Printf("\narray size after 1000 entries %d", a.size)
+	for i:=0; i<999; i++ {
+		a.Remove(1, nil)
+	}
+	a.Remove(0, nil)
+	fmt.Printf("\narray size after deletion of 1000 entries %d", a.size)
+}
