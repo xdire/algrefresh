a6c4e83abd2099c85436884e7f988939cf20d5df
Anton  Repin <arepin@cisco.com>
2019-11-19 12:24:39 -0800

    Files deleted for move

--- arrays/interface.go
+++
@@ -1,9 +0,0 @@
-package arrays
-
-type IArray interface {
-	Size() uint32
-	Add(item interface{})
-	Get(index uint32, receiver interface{})
-	Remove(index uint32, receiver interface{})
-	AddAtIndex(index uint32, item interface{})
-}
